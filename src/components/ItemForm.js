b7e575b1fa457e78cbee61a3d6a7fdf2f383928e
Joseph <joffan98@gmail.com>
2022-10-13 13:30:30 -0500

    solved!

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,42 @@
-import React from "react";
-import { v4 as uuid } from "uuid";
+import React, {useState} from "react";
+import { v4 as uuid } from "uuid"
 
-function ItemForm(props) {
+
+
+function ItemForm({onItemFormSubmit}) {
+
+  const [itemData, setItemData] = useState({
+    name:"",
+    category:"Produce",
+  })
+
+  const changeHandler = event => {
+    const key = event.target.name
+    const value = event.target.value
+    setItemData({...itemData, [key]:value})
+  }
+
+  const submitHandler = event => {
+    event.preventDefault()
+    const newItem = {
+      id: uuid(),
+      ...itemData
+    } 
+    onItemFormSubmit(newItem)
+  }
+
+
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={submitHandler}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" onChange={changeHandler}/>
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category" onChange={changeHandler}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
